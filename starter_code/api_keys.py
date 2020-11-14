2faecef945bd1ee760fe25f3ef262fe0b6a2d348
Bmaldonado44 <70544086+Bmaldonado44@users.noreply.github.com>
2020-11-14 14:30:38 -0600

    push homework

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "bd1fc6cccaaf3a9396239a553ce4d44d"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyALchReVLFGbHk2V_Y28i9AizBcqEZAiRw"
